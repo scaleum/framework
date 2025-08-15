83e5fbd6b0c13bfc4326983cfdbe0896d035f857
Informax <kirichenko.maxim@gmail.com>
2025-08-15 10:48:09 +0300

    Add methods for day start timestamp and interval offset
    Introduces getBeginOfDayTimestamp to retrieve the start of the day timestamp with an optional offset, and getUnixtimeWithOffset to apply interval modifications to a given Unix timestamp. These additions enhance time manipulation capabilities in TimeHelper.

--- src/Stdlib/Helpers/TimeHelper.php
+++ src/Stdlib/Helpers/TimeHelper.php
@@ -11,6 +11,8 @@ declare (strict_types = 1);
 
 namespace Scaleum\Stdlib\Helpers;
 
+use DateTime;
+
 /**
  * Representation of time in seconds
  */
@@ -28,7 +30,36 @@ class TimeHelper {
      */
     public static function getEndOfDayTimestamp(int $offset = 0) {
         $date     = date('Y-m-d', time() + $offset);
-        $endOfDay = $date . ' 23:59:59';
+        $endOfDay = "$date 23:59:59";
         return strtotime($endOfDay);
     }
+
+    /**
+     * Get the timestamp for the beginning of the day with an optional offset.
+     *
+     * This method returns the timestamp for the start of the current day,
+     * adjusted by the specified offset in days.
+     *
+     * @param int $offset The number of days to offset from the current day. Default is 0.
+     * @return int The timestamp for the beginning of the day with the specified offset.
+     */
+    public static function getBeginOfDayTimestamp(int $offset = 0)
+    {
+        $date = date('Y-m-d', time() + $offset);
+        $beginOfDay = "$date 00:00:01";
+        return strtotime($beginOfDay);
+    }
+
+    /**
+     * Get the Unix timestamp with a specified interval offset.
+     *
+     * @param int $unixtime The original Unix timestamp.
+     * @param string $interval The interval to offset the timestamp by (e.g., '+1 day', '-2 hours').
+     * @return int The Unix timestamp after applying the interval offset.
+     */
+    public static function getUnixtimeWithOffset($unixtime, string $interval) {
+        $date = new DateTime("@$unixtime");
+        $date->modify($interval);
+        return $date->getTimestamp();
+    }     
 }
