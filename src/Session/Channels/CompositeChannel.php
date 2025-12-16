248423fe44ed43fca54c018c11a971232b13690d
Informax <kirichenko.maxim@gmail.com>
2025-12-16 20:41:34 +0200

    Refactor session channels and enhance cookie options
    Refactored CompositeChannel to extend CookieChannel, simplifying code and reusing cookie logic. Enhanced CookieChannel with configurable cookie attributes (path, domain, secure, httpOnly, sameSite) and added validation for SameSite values.

--- src/Session/Channels/CompositeChannel.php
+++ src/Session/Channels/CompositeChannel.php
@@ -15,58 +15,38 @@ namespace Scaleum\Session\Channels;
 use Scaleum\Http\InboundRequest;
 use Scaleum\Http\OutboundResponse;
 use Scaleum\Session\Contracts\SessionChannelInterface;
-use Scaleum\Stdlib\Base\Hydrator;
 
 /**
  * CompositeChannel
  *
  * @author Maxim Kirichenko <kirichenko.maxim@gmail.com>
  */
-class CompositeChannel extends Hydrator implements SessionChannelInterface {
-    protected string $keyName = 'SID';
-
+class CompositeChannel extends CookieChannel implements SessionChannelInterface {
     public function fetchFromRequest(InboundRequest $request): ?string {
         // Prefer cookie
-        $cookies = $request->getCookieParams();
-        if (! empty($cookies[$this->keyName])) {
-            return rawurldecode((string) $cookies[$this->keyName]);
+        $result = parent::fetchFromRequest($request);
+        if ($result !== null) {
+            return $result;
         }
 
         // Fallback to header "X-<cookieName>"
-        $headerName = 'X-' . $this->keyName;
-        $value      = $request->getHeaderLine($headerName);
+        $headerName = "X-{$this->keyName}";
+        $result     = $request->getHeaderLine($headerName);
 
-        return $value !== '' ? $value : null;
+        return $result !== '' ? $result : null;
     }
 
     public function writeToResponse(OutboundResponse $response, string $id, ?int $ttl = null): void {
-        $value = rawurlencode($id);
+        parent::writeToResponse($response, $id, $ttl);
 
-        $attrs = [
-            'Path=/',
-            'HttpOnly',
-            'Secure',
-            'SameSite=Lax',
-        ];
-
-        if ($ttl > 0) {
-            $expires = gmdate('D, d M Y H:i:s', time() + $ttl) . ' GMT';
-            $attrs[] = 'Expires=' . $expires;
-            $attrs[] = 'Max-Age=' . (string) $ttl;
-        }
-
-        $cookie = sprintf('%s=%s; %s', $this->keyName, $value, implode('; ', $attrs));
-        $response->addHeader('Set-Cookie', $cookie);
-
-        $headerName = 'X-' . $this->keyName;
-        $response->setHeader($headerName, $id);
+        $headerName = "X-{$this->keyName}";
+        $response->addHeader($headerName, $id);
     }
 
     public function clearInResponse(OutboundResponse $response): void {
-        $cookie = sprintf('%s=deleted; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Path=/', $this->keyName);
-        $response->addHeader('Set-Cookie', $cookie);
+        parent::clearInResponse($response);
 
-        $headerName = 'X-' . $this->keyName;
+        $headerName = "X-{$this->keyName}";
         $response->addHeader($headerName, '');
     }
 }
--- src/Session/Channels/CookieChannel.php
+++ src/Session/Channels/CookieChannel.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types=1);
+declare (strict_types = 1);
 /**
  * This file is part of Scaleum Framework.
  *
@@ -15,57 +15,123 @@ use Scaleum\Http\InboundRequest;
 use Scaleum\Http\OutboundResponse;
 use Scaleum\Session\Contracts\SessionChannelInterface;
 use Scaleum\Stdlib\Base\Hydrator;
-
-
+use Scaleum\Stdlib\Exceptions\EInvalidArgumentException;
 
 /**
  * CookieSessionChannel
  *
  * @author Maxim Kirichenko <kirichenko.maxim@gmail.com>
  */
-class CookieChannel extends Hydrator implements SessionChannelInterface
-{
-    protected string $keyName = 'SID';
-    public function fetchFromRequest(InboundRequest $request): ?string
-    {
+class CookieChannel extends Hydrator implements SessionChannelInterface {
+    protected string $keyName  = 'SID';
+    protected string $path     = '/';
+    protected string $domain   = '';
+    protected bool $secure     = false;
+    protected bool $httpOnly   = false;
+    protected string $sameSite = 'Lax';
+
+    public function fetchFromRequest(InboundRequest $request): ?string {
         $cookies = $request->getCookieParams();
-        if (!empty($cookies[$this->keyName])) {
+        if (! empty($cookies[$this->keyName])) {
             return rawurldecode((string) $cookies[$this->keyName]);
         }
 
         return null;
     }
 
-    public function writeToResponse(OutboundResponse $response, string $id, ?int $ttl = null): void
-    {
+    public function writeToResponse(OutboundResponse $response, string $id, ?int $ttl = null): void {
+        $key   = rawurlencode($this->keyName);
         $value = rawurlencode($id);
 
-        $attrs = [
-            'Path=/',
-            'HttpOnly',
-            'Secure',
-            'SameSite=Lax',
+        $attributes = [
+            'Path'     => $this->getPath(),
+            'Domain'   => $this->getDomain(),
+            'Secure'   => $this->isSecure(),
+            'HttpOnly' => $this->isHttpOnly(),
+            'SameSite' => $this->getSameSite(),
         ];
 
         if ($ttl > 0) {
-            $expires = gmdate('D, d M Y H:i:s', time() + $ttl) . ' GMT';
-            $attrs[] = 'Expires=' . $expires;
-            $attrs[] = 'Max-Age=' . (string)$ttl;
+            $expires               = gmdate('D, d M Y H:i:s', time() + $ttl) . ' GMT';
+            $attributes['Expires'] = $expires;
+            $attributes['Max-Age'] = (string) $ttl;
         }
 
-        $cookie = sprintf('%s=%s; %s', $this->keyName, $value, implode('; ', $attrs));
+        $parts = ["{$key}={$value}"];
+        foreach ($attributes as $attrName => $attrValue) {
+            if (is_bool($attrValue)) {
+                if ($attrValue) {
+                    $parts[] = $attrName;
+                }
+            } elseif ($attrValue !== '') {
+                $parts[] = "{$attrName}={$attrValue}";
+            }
+        }
+
+        $response->addHeader('Set-Cookie', implode('; ', $parts));
+    }
+
+    public function clearInResponse(OutboundResponse $response): void {
+        $cookie = sprintf(
+            '%s=deleted; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Path=/',
+            rawurlencode($this->keyName)
+        );
 
         $response->addHeader('Set-Cookie', $cookie);
     }
 
-    public function clearInResponse(OutboundResponse $response): void
-    {
-        $cookie = sprintf(
-            '%s=deleted; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Path=/',
-            $this->keyName
-        );
+    public function setPath(string $path): static {
+        $this->path = $path;
+        return $this;
+    }
 
-        $response->addHeader('Set-Cookie', $cookie);
-    }    
+    public function getPath(): string {
+        return $this->path;
+    }
+
+    public function setDomain(string $domain): static {
+        $this->domain = $domain;
+        return $this;
+    }
+
+    public function getDomain(): string {
+        return $this->domain;
+    }
+
+    public function setSecure(bool $secure): static {
+        $this->secure = $secure;
+        return $this;
+    }
+
+    public function isSecure(): bool {
+        return $this->secure;
+    }
+
+    public function setHttpOnly(bool $httpOnly): static {
+        $this->httpOnly = $httpOnly;
+        return $this;
+    }
+
+    public function isHttpOnly(): bool {
+        return $this->httpOnly;
+    }
+
+    public function setSameSite(string $sameSite): static {
+        $allowed = ['Strict', 'Lax', 'None'];
+        if (! in_array($sameSite, $allowed, true)) {
+            throw new EInvalidArgumentException(sprintf('Unacceptable SameSite value: %s. Allowed values: %s.', $sameSite, implode(', ', $allowed)));
+        }
+
+        $this->sameSite = $sameSite;
+        if ($sameSite === 'None' && ! $this->isSecure()) {
+            $this->setSecure(true);
+        }
+
+        return $this;
+    }
+
+    public function getSameSite(): string {
+        return $this->sameSite;
+    }
 }
 /** End of CookieSessionChannel **/
